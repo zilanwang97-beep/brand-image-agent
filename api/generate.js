c8ce7031a39b71d6c977cc5f2e3261e8bd655bd3
zilanwang97-beep <zilanwang97@gmail.com>
2026-06-04 14:57:52 -0700

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -1,7 +1,7 @@
 const CORS_HEADERS = {
-  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Origin": "https://v0-brand-image-tool.vercel.app",
   "Access-Control-Allow-Methods": "POST, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
   "Content-Type": "application/json",
 };
 
